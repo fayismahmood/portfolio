e3b6a3e4c442ea3dd7f3a9b2632d618868c9bd74
fayismahmood <fmthottathil@gmail.com>
2022-03-22 23:17:46 +0530

    fadfadsf

--- configs.js
+++ configs.js
@@ -1,3 +1,3 @@
 const dev=process.env.NODE_ENV!=="production"
 
-export const server=dev?"http://localhost:3000/":"portfolio-steel-theta-64.vercel.app"
+export const server=dev?"http://localhost:3000/":"http://portfolio-steel-theta-64.vercel.app"
